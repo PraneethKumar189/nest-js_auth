52feee746295b50e77564c08c06fff20ce74d1bc
Praneeth Kumar <124180295+PraneethKumar189@users.noreply.github.com>
2025-01-03 21:49:02 +0530

    added JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,20 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDTO } from './dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private prisma:PrismaService){}
+    constructor(private prisma:PrismaService,private jwtservice:JwtService){}
 
     hashData(data:string){
          return bcrypt.hash(data,10);
+    }
+
+   async getTokens(userId:number,email:string){
+    const accessToken =
+
     }
 
     async signupLocal(dto:AuthDTO):Promise<Tokens>{
